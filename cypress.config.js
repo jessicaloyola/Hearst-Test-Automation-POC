ffe3bb5ee5381258f4835448e3b129bfcc1a50a0
Jorge Orduño Buenrostro <jorge.orduno@MacBook-Pro-de-Jorge.local>
2022-11-03 00:51:03 -0600

    Configs were added, also footer its completed

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'My Test Suite',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/FooterTests.cy.js
+++ cypress/e2e/FooterTests.cy.js
@@ -6,7 +6,22 @@ describe('Footer Testing for PoC', () => {
 
 
     it('Footer Validation', () => {
-        cy.visit('/')
+        cy.visit('/life/food/a34978134/patti-labelle-stuffing-recipe/')
         footer.getDoNotSellMyinfoButton().should('be.visible')
+        footer.getBeingGreen().should('be.visible')
+        footer.getCaliforniaPrivacyNotice().should('be.visible')
+        footer.getContactUs().should('be.visible')
+        footer.getFacebook().should('be.visible')
+        footer.getGiveAGift().should('be.visible')
+        footer.getHearstLogoBotton().should('be.visible')
+        footer.getInstagram().should('be.visible')
+        footer.getInterestBasedAds().should('be.visible')
+        footer.getNewsletter().should('be.visible')
+        footer.getOprahCopyRight().should('be.visible')
+        footer.getPinteres().should('be.visible')
+        footer.getPressRoom().should('be.visible')
+        footer.getPrivacyNotice().should('be.visible')
+        footer.getTermsOfUse().should('be.visible')
+        footer.getTwiiter().should('be.visible')
     })
 })
